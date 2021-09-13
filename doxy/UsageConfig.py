c9e9f5468edc4749e855b1b68d7feefe4016ff7d
Boris <boris.fox.solovyev@gmail.com>
2021-09-13 17:17:34 +0300

    Update UsageConfig.py

--- doxy/UsageConfig.py
+++ doxy/UsageConfig.py
@@ -1,14 +1,15 @@
 from Components.Harddisk import harddiskmanager
 from Components.Console import Console
-from config import ConfigSubsection, ConfigYesNo, config, ConfigSelection, ConfigText, ConfigNumber, ConfigSet, ConfigLocations, ConfigSelectionNumber, ConfigClock, ConfigSlider, ConfigEnableDisable, ConfigSubDict, ConfigDictionarySet, ConfigInteger, ConfigPassword, ConfigIP, NoSave
+from config import ConfigSubsection, ConfigYesNo, config, ConfigSelection, ConfigText, ConfigNumber, ConfigSet, ConfigLocations, ConfigSelectionNumber, ConfigClock, ConfigSlider, ConfigEnableDisable, ConfigSubDict, ConfigDictionarySet, ConfigInteger, ConfigPassword, ConfigIP, NoSave, ConfigBoolean
 from Tools.Directories import defaultRecordingLocation, SCOPE_HDD, SCOPE_TIMESHIFT, fileContains, resolveFilename, fileHas
 from enigma import setTunerTypePriorityOrder, setPreferredTuner, setSpinnerOnOff, setEnableTtCachingOnOff, eEnv, eDVBDB, Misc_Options, eBackgroundFileEraser, eServiceEvent, eDVBLocalTimeHandler, eEPGCache
 from Components.About import GetIPsFromNetworkInterfaces
 from Components.NimManager import nimmanager
 from Components.ServiceList import refreshServiceList
 from SystemInfo import SystemInfo
-import os
-import time
+from os import mkdir, remove
+from os.path import exists, islink, join as pathjoin, normpath
+import os, time, locale, skin
 
 
 def InitUsageConfig():
@@ -157,7 +158,7 @@ def InitUsageConfig():
 
 	if not exists(resolveFilename(SCOPE_HDD)):
 		try:
-			os.mkdir(resolveFilename(SCOPE_HDD), 0755)
+			mkdir(resolveFilename(SCOPE_HDD), 0755)
 		except (IOError, OSError):
 			pass
 	defaultValue = resolveFilename(SCOPE_HDD)
@@ -188,7 +189,7 @@ def InitUsageConfig():
 	config.usage.instantrec_path.save()
 	if not exists(resolveFilename(SCOPE_TIMESHIFT)):
 		try:
-			os.mkdir(resolveFilename(SCOPE_TIMESHIFT), 0755)
+			mkdir(resolveFilename(SCOPE_TIMESHIFT), 0755)
 		except:
 			pass
 	defaultValue = resolveFilename(SCOPE_TIMESHIFT)
