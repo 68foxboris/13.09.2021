78bba70f9ad08e8cbf1e138da63d3c84a5596391
Boris <boris.fox.solovyev@gmail.com>
2021-10-10 08:25:07 +0300

    Update bsod.cpp

--- m4/bsod.cpp
+++ m4/bsod.cpp
@@ -157,9 +157,14 @@ void bsodFatal(const char *component)
 	std::string crashlog_name;
 	std::ostringstream os;
 	std::ostringstream os_text;
+	time_t t = time(0);
+	struct tm tm;
+	char tm_str[32];
+	localtime_r(&t, &tm);
+	strftime(tm_str, sizeof(tm_str), "%Y-%m-%d_%H-%M-%S", &tm);
 	os << getConfigString("config.crash.debugPath", "/home/root/logs/");
 	os << "enigma2_crash_";
-	os << time(0);
+	os << tm_str;
 	os << ".log";
 	crashlog_name = os.str();
 	f = fopen(crashlog_name.c_str(), "wb");
